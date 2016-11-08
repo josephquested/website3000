16167b9ab5096c27761a5fd0780f86b641874e79
josephquested <josephquested@gmail.com>
2016-11-08 15:09:24 +1300

    initial firebase integration

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ var app = require('express')()
 var templit = require('templit')
 var port = process.env.PORT || 3000
 
+var admin = require("firebase-admin");
+var serviceAccount = require("./firebase/serviceAccountKey.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://website3000-28bb3.firebaseio.com/"
+});
+
 app.engine('js', templit)
 app.set('view engine', 'js')
 
